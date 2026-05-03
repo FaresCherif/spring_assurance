76dcb7c506d3516af1153af5ed4d316237fd01da
Fares <fares.cherif@etu.unilim.fr>
2026-05-03 13:04:24 +0200

    first commit

---
+++ src/test/java/com/example/spring_assurance/SpringAssuranceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_assurance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAssuranceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
