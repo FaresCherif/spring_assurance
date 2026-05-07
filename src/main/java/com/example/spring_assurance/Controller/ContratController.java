1d146bbd23443cd2e47ff051ff3312c9ea544076
Fares <fares.cherif@etu.unilim.fr>
2026-05-07 22:26:24 +0200

    add route update contrat

--- src/main/java/com/example/spring_assurance/Controller/ContratController.java
+++ src/main/java/com/example/spring_assurance/Controller/ContratController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -44,5 +45,10 @@ public class ContratController {
     @DeleteMapping("/{id}")
     public void delete(@PathVariable Long id){
         contratService.delete(id);
+    }
+
+    @PutMapping("/update")
+    public Contrat update(@RequestBody Contrat contrat){
+        return contratService.save(contrat);
     }
 }
